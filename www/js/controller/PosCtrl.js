9a183a89336230d1f7ca56b1c828b1af390e54fa
killerwhale <ayoub.bahaj@gmail.com>
2017-11-19 16:11:23 +0100

    add semicolones

--- www/js/controller/PosCtrl.js
+++ www/js/controller/PosCtrl.js
@@ -11,7 +11,7 @@ $scope.panel = 0;
 startRangingBeacons();
  //orientationDevice();
 
-  }
+};
   /* automatic search */
    $scope.selectedRoom = "";
 
@@ -73,7 +73,7 @@ function startRangingBeacons(){
           function(beaconInfo) {
 
             //Array to stock Data from Beacons what we found
-         
+
 
           var heatMap = [];
           var der='';
@@ -148,18 +148,18 @@ function startRangingBeacons(){
 
 
 
-           
+
             $.each(beaconInfo.beacons, function(key, beacon){
                       beaconInfo.beacons.sort(function(beacon1, beacon2) {
                       return beacon1.rssi < beacon2.rssi; });
                       var string_UUID = beacon.proximityUUID;
 											var lengthUUID = string_UUID.length;
 											var lastUUID = string_UUID.substr(lengthUUID - 4);
-                      if( lastUUID == '1218' ) rssi_1218.push(beacon.rssi)
-											if( lastUUID == '1219' ) rssi_1219.push(beacon.rssi)
-											if( lastUUID == '1221' ) rssi_1221.push(beacon.rssi)
-											if( lastUUID == '1222' ) rssi_1222.push(beacon.rssi)
-											if( lastUUID == '1211' ) rssi_1211.push(beacon.rssi)
+                      if( lastUUID == '1218' ) rssi_1218.push(beacon.rssi);
+											if( lastUUID == '1219' ) rssi_1219.push(beacon.rssi);
+											if( lastUUID == '1221' ) rssi_1221.push(beacon.rssi);
+											if( lastUUID == '1222' ) rssi_1222.push(beacon.rssi);
+											if( lastUUID == '1211' ) rssi_1211.push(beacon.rssi);
                       //t_beacon.push({ "uuid" :beacon.proximityUUID,"rssi" : beacon.rssi});
 
 
@@ -168,7 +168,7 @@ function startRangingBeacons(){
 
 
               //console.log("heatMap :"+heatMap);
-             
+
               if(rssi_1218.length>0){
                 var sum_1218 = rssi_1218.reduce(sum_array);
                 var mean_1218 = sum_1218 / rssi_1218.length;
@@ -178,7 +178,7 @@ function startRangingBeacons(){
                 //console.log('distance of 18 ='+dist_1218);
                 t_beacon.push({"uuid":"b9407f30-f5f8-466e-aff9-25556b571218", "rssi": final_rssi_1218,"distance":dist_1218,"x":BeaconsTab[0].x,"y":BeaconsTab[0].y});
               }
-              
+
               if(rssi_1219.length>0){
                 var sum_1219 = rssi_1219.reduce(sum_array);
                 var mean_1219 = sum_1219 / rssi_1219.length;
@@ -188,7 +188,7 @@ function startRangingBeacons(){
                 //console.log('distance of 19 ='+dist_1219);
                 t_beacon.push({"uuid":"b9407f30-f5f8-466e-aff9-25556b571219", "rssi": final_rssi_1219,"distance":dist_1219,"x":BeaconsTab[1].x,"y":BeaconsTab[1].y});
               }
-							
+
               if(rssi_1221.length>0){
                 var sum_1221 = rssi_1221.reduce(sum_array);
                 var mean_1221 = sum_1221 / rssi_1221.length;
@@ -198,7 +198,7 @@ function startRangingBeacons(){
                 //console.log('distance of 21 ='+dist_1221);
                 t_beacon.push({"uuid":"b9407f30-f5f8-466e-aff9-25556b571221", "rssi": final_rssi_1221,"distance":dist_1221,"x":BeaconsTab[2].x,"y":BeaconsTab[2].y});
               }
-							
+
               if(rssi_1222.length>0){
                 var sum_1222 = rssi_1222.reduce(sum_array);
                 var mean_1222 = sum_1222 / rssi_1222.length;
@@ -208,7 +208,7 @@ function startRangingBeacons(){
                 //console.log('distance of 22 ='+dist_1222);
                 t_beacon.push({"uuid":"b9407f30-f5f8-466e-aff9-25556b571222", "rssi": final_rssi_1222,"distance":dist_1222,"x":BeaconsTab[3].x,"y":BeaconsTab[3].y});
               }
-							
+
 							if(rssi_1211.length>0){
               var sum_1211 = rssi_1211.reduce(sum_array);
 							var mean_1211 = sum_1211 / rssi_1211.length;
@@ -216,11 +216,11 @@ function startRangingBeacons(){
 							var final_rssi_1211 = final_rssi(rssi_1211, mean_1211, standat_1211);
               var dist_1211 = distance_rssi(final_rssi_1211);
               //console.log('distance of 11 ='+dist_1211);
-              
+
               t_beacon.push({"uuid":"b9407f30-f5f8-466e-aff9-25556b571211", "rssi": final_rssi_1211,"distance":dist_1211,"x":BeaconsTab[4].x,"y":BeaconsTab[4].y});
              }
-              
-              
+
+
               /*console.log("distance of 1218 is : "+dist_1218+"\n");
               console.log("distance of 1219 is : "+dist_1219+"\n");
               console.log("distance of 1221 is : "+dist_1221+"\n");
@@ -228,7 +228,7 @@ function startRangingBeacons(){
 
               // console.log("1218 x :"+BeaconsTab[0].x+" 1218 y: "+BeaconsTab[0].y+"\n");
              //t_beacon.push(elem);
-              
+
               t_beacon.sort(function(beacon1, beacon2) {
                       return beacon1.distance > beacon2.distance; });
 
@@ -244,19 +244,19 @@ function startRangingBeacons(){
               if(t_beacon.length>2)
                 {
                   var pos_device = calculate_Tri(t_beacon);
-              
+
               console.log("position device x :"+pos_device.x+" position device y: "+pos_device.y);
               // alert("after pos_device");
 
-              // start show the position on the map 
+              // start show the position on the map
 
               $('#ShowBeacons').append("<p> x :"+pos_device.x+" y: "+pos_device.y+'</p>');*/
-              
+
 
             //alert("the length of t_beacon is :"+t_beacon.length);
-            
+
             // algorthme of heatMap
-            
+
             var errorTab;
             var min=Math.pow(10,20);
             var err=0;
@@ -272,7 +272,7 @@ function startRangingBeacons(){
                     for(j=0;j<heatMap[i].listBeacons.length;j++){
                       //console.log("rssi t_beacon : "t_beacon[k].rssi+" ,rssi of listBeacons :"+heatMap.listBeacons[j].rssi);
                         if(t_beacon[k].uuid.toLowerCase() == heatMap[i].listBeacons[j].uuid.toLowerCase()){
-                          
+
                           gefunden=true;
                           err = Math.pow(Math.abs(heatMap[i].listBeacons[j].rssi)-Math.abs(t_beacon[k].rssi),2);
                           //console.log("err :"+err);
@@ -295,15 +295,15 @@ function startRangingBeacons(){
 
             var lengthZone = zones.length;
             for(i =0;i<lengthZone;i++){
-              if(zones[i].zone == Zonefinale) 
+              if(zones[i].zone == Zonefinale)
                 pos_device = { "x": zones[i].x,  "y": zones[i].y };
             }
              d3.select("#Device_position").transition().duration(800)
                     .attr("visibility","visible")
                     .attr("cx",pos_device.x)
                     .attr("cy",pos_device.y );
-            
-            // end heat map lagorithm 
+
+            // end heat map lagorithm
 
             t_beacon  =[];
             rssi_1218 =[];
@@ -312,14 +312,14 @@ function startRangingBeacons(){
             rssi_1222 =[];
             rssi_1211 =[];
             e = 0;
-            
-           
+
+
              //d3.select(".zones").style({"fill":"#dddd9d"});
              //d3.select("#"+Zonefinale).style({"fill":"#e74c3c"});
 
         }
       },
-          function(errorMessage) {  alert('Ranging error: ' + errorMessage)  });
+          function(errorMessage) {  alert('Ranging error: ' + errorMessage);  });
 
 
       }//End Of startRangingBeacons
@@ -369,12 +369,12 @@ function startRangingBeacons(){
       var cal= -66;
       var dist;
       if(cal<rssi_p){
-          dist = Math.pow(10,rssi_p/cal)
+          dist = Math.pow(10,rssi_p/cal);
       }
         else{
-          dist = (0.9*Math.pow(7.71,rssi_p/cal)+0.11)*0.001
+          dist = (0.9*Math.pow(7.71,rssi_p/cal)+0.11)*0.001;
         }
-      return dist
+      return dist;
      }
 
       //Begin Of accelerationDevice get acceleration Device
@@ -441,7 +441,7 @@ function startRangingBeacons(){
         function calculate_Tri2(beacons){
 
           //alert("x1 :"+x1+" y1 :"+y1+"x2 :"+x2+"y2 :"+y2+"x3 :"+x3+"y3 :"+y3);
-             
+
              var x1 = beacons[0].x;
              var y1 = beacons[0].y;
              var x2 = beacons[1].x;
@@ -507,10 +507,10 @@ d3.select("#svg8").append("path")
 function findWay(){
 
   d3.selectAll(".knoten").attr("visibility", "hidden");
-    
+
   var id_room = $('#room').val().split(" ").join("").toLowerCase();
   id_room = zimmerNameId[id_room];
-  
+
   graph = new Graph(map);
   var shortWay = [];
   shortWay = graph.findShortestPath('circle17',id_room);
